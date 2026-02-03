a4520f0e06a57cb2663f24b814a13f77c778316d
Vladimir <89457026+vkovalyov@users.noreply.github.com>
2026-02-03 17:35:58 +0700

    фикс после ребейза

--- app/src/main/java/com/example/shoppinglist/ui/theme/FontFamily.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shoppinglist.ui.theme
-
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import com.example.shoppinglist.R
-
-val Roboto = FontFamily(
-    Font(R.font.roboto)
-)
