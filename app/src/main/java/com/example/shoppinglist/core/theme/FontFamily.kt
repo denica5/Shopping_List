1f3b507f0afc63ea5223bda7e9740676ec1cf563
Vladimir <89457026+vkovalyov@users.noreply.github.com>
2026-02-03 17:14:29 +0700

    добавлены цвета, шрифты, базовые размеры и цвета для светлой/темной тем

---
+++ app/src/main/java/com/example/shoppinglist/core/theme/FontFamily.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.shoppinglist.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto)
+)
