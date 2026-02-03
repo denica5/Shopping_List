a7fbe7bc4bfd4e3df0038aa04de5e67111e58d31
Vladimir <89457026+vkovalyov@users.noreply.github.com>
2026-02-03 17:14:29 +0700

    добавлены цвета, шрифты, базовые размеры и цвета для светлой/темной тем

--- app/src/main/java/com/example/shoppinglist/ui/theme/Theme.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.shoppinglist.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,31 +11,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = AppColors.BackgroundDark,
+    surface = AppColors.SurfaceDark,
+    onSurface = AppColors.OnSurfaceDark,
+    onSurfaceVariant = AppColors.OnSurfaceVariantDark,
+    primaryContainer = AppColors.PrimaryContainerDark,
+    onPrimaryContainer = AppColors.OnPrimaryContainerDark,
+    surfaceContainerHigh = AppColors.SurfaceContainerHighDark,
+    secondary = AppColors.SecondaryDark,
+    primary = AppColors.PrimaryDark,
+    secondaryContainer = AppColors.SecondaryContainerDark,
+    onSecondaryContainer = AppColors.OnSecondaryContainerDark,
+    onPrimary = AppColors.OnPrimaryDark,
+    surfaceContainerHighest = AppColors.SurfaceContainerHighestDark,
+    outlineVariant = AppColors.OutlineVariantDark,
+    surfaceContainerLow = AppColors.SurfaceContainerLowDark,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = AppColors.BackgroundLight,
+    surface = AppColors.SurfaceLight,
+    onSurface = AppColors.OnSurfaceLight,
+    onSurfaceVariant = AppColors.OnSurfaceVariantLight,
+    primaryContainer = AppColors.PrimaryContainerLight,
+    onPrimaryContainer = AppColors.OnPrimaryContainerLight,
+    surfaceContainerHigh = AppColors.SurfaceContainerHighLight,
+    primary = AppColors.PrimaryLight,
+    secondaryContainer = AppColors.SecondaryContainerLight,
+    onSecondaryContainer = AppColors.OnSecondaryContainerLight,
+    onPrimary = AppColors.OnPrimaryLight,
+    surfaceContainerHighest = AppColors.SurfaceContainerHighestLight,
+    outlineVariant = AppColors.OutlineVariantLight,
+    surfaceContainerLow = AppColors.SurfaceContainerLowLight,
 )
 
 @Composable
 fun ShoppingListTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
