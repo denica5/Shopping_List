d5f98383de3a33401d820330eca7048056da6ea6
denica5 <gleb.rudko@bk.ru>
2026-02-18 12:08:14 +0300

    исправления iteration

--- app/src/main/java/com/example/shoppinglist/features/productLists/presentation/composables/EmptyContent.kt
+++ app/src/main/java/com/example/shoppinglist/features/productLists/presentation/composables/EmptyContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -18,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.shoppinglist.R
 
+@Stable
 @Composable
 fun EmptyContent() {
     Column(
